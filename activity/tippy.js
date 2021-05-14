dda95c593b4cb2db61531f1a20f2df9c07576df0
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-14 23:34:24 +0530

    code added upto tippys

---
+++ activity/tippy.js
@@ -0,0 +1,60 @@
+tippy('[class="left active_btn"]', {
+    content: "I align text to left",
+    arrow:true,
+    placement:'left',
+  });
+
+
+tippy('[class="right"]', {
+    content: "I align text to right",
+    arrow:true,
+    placement:'right',
+  });
+
+
+tippy('[class="center"]', {
+    content: "I align text to center",
+    arrow:true,
+  });
+
+
+  tippy('[class="font_family"]', {
+    content: "I regulates font style",
+    arrow:true,
+  });
+
+
+  tippy('[class="font_size"]', {
+    content: "I regulates font size",
+    arrow:true,
+  });
+
+
+  tippy('[class="bold"]', {
+    content: "I make text 𝐁𝐎𝐋𝐃",
+    arrow:true,
+  });
+
+
+  tippy('[class="italic"]', {
+    content: "I make text 𝘐𝘵𝘢𝘭𝘪𝘤",
+    arrow:true,
+  });
+
+
+  tippy('[class="underline"]', {
+    content: "I make text u͟n͟d͟e͟r͟l͟i͟n͟e͟",
+    arrow:true,
+  });
+
+
+  tippy('[id="color"]', {
+    content: "I change text color",
+    arrow:true,
+  });
+
+
+  tippy('[id="bg_color"]', {
+    content: "I change background color",
+    arrow:true,
+  });
